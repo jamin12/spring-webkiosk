a4f6ba2d9cbcb6c232a6a4630202cce8b8d506da
'jamin12' <'rudals9901@naver.com'>
2022-01-03 23:21:26 +0900

    start

---
+++ webkiosk/src/main/java/com/example/webkiosk/WebkioskApplication.java
@@ -0,0 +1,13 @@
+package com.example.webkiosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebkioskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebkioskApplication.class, args);
+	}
+
+}
---
+++ webkiosk/src/test/java/com/example/webkiosk/WebkioskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webkiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebkioskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
