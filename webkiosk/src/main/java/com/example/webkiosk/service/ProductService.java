28873f0011e93f3acd96ea20c1c9a3a3e32659d8
jamin12 <rudals9901@naver.com>
2022-02-14 00:07:32 +0900

    make service interface

--- webkiosk/src/main/java/com/example/webkiosk/service/ProductService.java
+++ webkiosk/src/main/java/com/example/webkiosk/service/ProductService.java
@@ -1,18 +1,9 @@
 //강경민
 package com.example.webkiosk.service;
 
-import java.util.List;
-
-import javax.transaction.Transactional;
-
-import com.example.webkiosk.entity.Product;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import lombok.extern.slf4j.Slf4j;
-
 @Service
-@Slf4j
-public class ProductService {
+public interface ProductService {
 
 }
